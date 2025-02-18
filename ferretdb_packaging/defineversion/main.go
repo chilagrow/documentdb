05af03fe61619ac624b1702fb7c7da0930f6afd7
Chi Fujii <chi.fujii@ferretdb.io>
2025-02-18 10:32:53 +0900

    add version tool

---
+++ ferretdb_packaging/defineversion/main.go
@@ -0,0 +1,207 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main defines debian version number of DocumentDB for CI builds.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"slices"
+	"strings"
+
+	"github.com/sethvargo/go-githubactions"
+)
+
+func main() {
+	controlFileF := flag.String("control-file", "../pg_documentdb/documentdb.control", "pg_documentdb/documentdb.control file path")
+	flag.Parse()
+
+	action := githubactions.New()
+
+	if *controlFileF == "" {
+		action.Fatalf("%s", "-control-file flag is empty.")
+	}
+
+	version, err := controlVersion(*controlFileF)
+
+	debugEnv(action)
+
+	res, err := define(version, action.Getenv)
+	if err != nil {
+		action.Fatalf("%s", err)
+	}
+
+	setResults(action, res)
+}
+
+// controlDefaultVer matches the default_version field in control file,
+// see pg_documentdb_core/documentdb_core.control.
+var controlDefaultVer = regexp.MustCompile(`default_version\s*=\s*'([0-9]+\.[0-9]+-[0-9]+)'`)
+
+// documentDBVer is the version syntax used by documentdb.
+// For documentdb.control file, the version is in the format of `0.100-0`.
+// For release tags, it has a leading `v` such as `v0.100-0`.
+//
+//nolint:lll // for readibility
+var documentDBVer = regexp.MustCompile(`^v?(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)-(?P<patch>0|[1-9]\d*)$`)
+
+// debianVer matches allowed characters for debian package version,
+// see https://www.debian.org/doc/debian-policy/ch-controlfields.html#version.
+var debianVer = regexp.MustCompile(`[^A-Za-z0-9~.+]`)
+
+// debugEnv logs all environment variables that start with `GITHUB_` or `INPUT_`
+// in debug level.
+func debugEnv(action *githubactions.Action) {
+	res := make([]string, 0, 30)
+
+	for _, l := range os.Environ() {
+		if strings.HasPrefix(l, "GITHUB_") || strings.HasPrefix(l, "INPUT_") {
+			res = append(res, l)
+		}
+	}
+
+	slices.Sort(res)
+
+	action.Debugf("Dumping environment variables:")
+
+	for _, l := range res {
+		action.Debugf("\t%s", l)
+	}
+}
+
+// controlVersion returns the default_version field from the control file,
+// see pg_documentdb_core/documentdb_core.control.
+func controlVersion(f string) (string, error) {
+	b, err := os.ReadFile(f)
+	if err != nil {
+		return "", err
+	}
+
+	match := controlDefaultVer.FindSubmatch(b)
+	if len(match) != 2 {
+		return "", fmt.Errorf("control file did not find default_version match file: %s", f)
+	}
+
+	version := string(match[1])
+
+	return version, nil
+}
+
+// define extracts debian version number from the environment variables of GitHub Actions.
+// If the release tag is set, it checks the tag matches the control version
+// and returns an error on mismatch.
+func define(controlDefaultVersion string, getenv githubactions.GetenvFunc) (string, error) {
+	version, err := parseVersion(controlDefaultVersion)
+	if err != nil {
+		return "", err
+	}
+
+	var res string
+
+	switch event := getenv("GITHUB_EVENT_NAME"); event {
+	case "pull_request", "pull_request_target":
+		branch := strings.ToLower(getenv("GITHUB_HEAD_REF"))
+		res = defineForPR(version, branch)
+
+	case "push", "schedule", "workflow_run":
+		refName := strings.ToLower(getenv("GITHUB_REF_NAME"))
+
+		switch refType := strings.ToLower(getenv("GITHUB_REF_TYPE")); refType {
+		case "branch":
+			res, err = defineForBranch(version, refName)
+
+		case "tag":
+			res, err = defineForTag(version, refName)
+
+		default:
+			err = fmt.Errorf("unhandled ref type %q for event %q", refType, event)
+		}
+
+	default:
+		err = fmt.Errorf("unhandled event type %q", event)
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	if res == "" {
+		panic("both res and err are nil")
+	}
+
+	return res, nil
+}
+
+// defineForPR defines debian version number for pull requests.
+// It replaces branch name with allowed chars of debian package version.
+func defineForPR(version, branch string) string {
+	// for branches like "dependabot/submodules/XXX"
+	parts := strings.Split(branch, "/")
+	branch = parts[len(parts)-1]
+	branch = debianVer.ReplaceAllString(branch, "~")
+
+	return fmt.Sprintf("%s~pr~%s", version, branch)
+}
+
+// defineForBranch defines debian version number for branch builds.
+func defineForBranch(version, branch string) (string, error) {
+	switch branch {
+	case "main", "ferretdb":
+		return fmt.Sprintf("%s~%s", version, branch), nil
+	default:
+		return "", fmt.Errorf("unhandled branch %q", branch)
+	}
+}
+
+// defineForTag defines debian version number for release build.
+// It returns an error if tag version does not match the control version.
+func defineForTag(controlVersion string, tag string) (string, error) {
+	tagVersion, err := parseVersion(tag)
+	if err != nil {
+		return "", err
+	}
+
+	if tagVersion != controlVersion {
+		return "", fmt.Errorf("version in control file and release tag mismatch control:%s tag:%s", controlVersion, tagVersion)
+	}
+
+	return tagVersion, nil
+}
+
+// parseVersion parses the version string in the format `0.100-0` or `v0.100-0` and
+// returns a normalized version string in `0.100.0` format.
+func parseVersion(version string) (string, error) {
+	match := documentDBVer.FindStringSubmatch(version)
+	if match == nil || len(match) != documentDBVer.NumSubexp()+1 {
+		return "", fmt.Errorf("unexpected version syntax %q", version)
+	}
+
+	major := match[documentDBVer.SubexpIndex("major")]
+	minor := match[documentDBVer.SubexpIndex("minor")]
+	patch := match[documentDBVer.SubexpIndex("patch")]
+
+	return major + "." + minor + "." + patch, nil
+}
+
+// setResults sets action output parameters, summary, etc.
+func setResults(action *githubactions.Action, res string) {
+	output := fmt.Sprintf("version: %s", res)
+
+	action.AddStepSummary(output)
+	action.Infof("%s", output)
+	action.SetOutput("version", res)
+}
---
+++ ferretdb_packaging/defineversion/main_test.go
@@ -0,0 +1,230 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/sethvargo/go-githubactions"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// getEnvFunc implements [os.Getenv] for testing.
+func getEnvFunc(t *testing.T, env map[string]string) func(string) string {
+	t.Helper()
+
+	return func(key string) string {
+		val, ok := env[key]
+		require.True(t, ok, "missing key %q", key)
+
+		return val
+	}
+}
+func TestDefine(t *testing.T) {
+	for name, tc := range map[string]struct {
+		env            map[string]string
+		defaultVersion string // pg_documentdb/documentdb.control file's default_version field
+		expected       string
+	}{
+		"pull_request": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "pull_request",
+				"GITHUB_HEAD_REF":   "define-docker-tag",
+				"GITHUB_REF_NAME":   "1/merge",
+				"GITHUB_REF_TYPE":   "branch",
+			},
+			defaultVersion: "0.100-0",
+			expected:       "0.100.0~pr~define~docker~tag",
+		},
+
+		"pull_request/dependabot": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "pull_request",
+				"GITHUB_HEAD_REF":   "dependabot/submodules/tests/mongo-go-driver-29d768e",
+				"GITHUB_REF_NAME":   "58/merge",
+				"GITHUB_REF_TYPE":   "branch",
+			},
+			defaultVersion: "0.100-0",
+			expected:       "0.100.0~pr~mongo~go~driver~29d768e",
+		},
+
+		"pull_request_target": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "pull_request_target",
+				"GITHUB_HEAD_REF":   "define-docker-tag",
+				"GITHUB_REF_NAME":   "main",
+				"GITHUB_REF_TYPE":   "branch",
+			},
+			defaultVersion: "0.100-0",
+			expected:       "0.100.0~pr~define~docker~tag",
+		},
+
+		"push/main": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "main",
+				"GITHUB_REF_TYPE":   "branch",
+			},
+			defaultVersion: "0.100-0",
+			expected:       "0.100.0~main",
+		},
+		"push/ferretdb": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "ferretdb",
+				"GITHUB_REF_TYPE":   "branch",
+			},
+			defaultVersion: "0.100-0",
+			expected:       "0.100.0~ferretdb",
+		},
+		"push/other": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "releases",
+				"GITHUB_REF_TYPE":   "other", // not main or ferretdb branch
+			},
+		},
+
+		"push/tag/release1": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "v0.100-0",
+				"GITHUB_REF_TYPE":   "tag",
+			},
+			defaultVersion: "0.100-0",
+			expected:       "0.100.0",
+		},
+
+		"push/tag/mismatch": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "v0.100-1", // default version and tag mismatch
+				"GITHUB_REF_TYPE":   "tag",
+			},
+			defaultVersion: "0.100-0",
+		},
+
+		"push/tag/wrong": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "push",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "0.100-0", // no leading v
+				"GITHUB_REF_TYPE":   "tag",
+			},
+		},
+
+		"schedule": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "schedule",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "main",
+				"GITHUB_REF_TYPE":   "branch",
+			},
+			defaultVersion: "0.100-0",
+			expected:       "0.100.0~main",
+		},
+
+		"workflow_run": {
+			env: map[string]string{
+				"GITHUB_EVENT_NAME": "workflow_run",
+				"GITHUB_HEAD_REF":   "",
+				"GITHUB_REF_NAME":   "main",
+				"GITHUB_REF_TYPE":   "branch",
+			},
+			defaultVersion: "0.100-0",
+			expected:       "0.100.0~main",
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			actual, err := define(tc.defaultVersion, getEnvFunc(t, tc.env))
+			if tc.expected == "" {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestResults(t *testing.T) {
+	dir := t.TempDir()
+
+	summaryF, err := os.CreateTemp(dir, "summary")
+	require.NoError(t, err)
+	defer summaryF.Close() //nolint:errcheck // temporary file for testing
+
+	outputF, err := os.CreateTemp(dir, "output")
+	require.NoError(t, err)
+	defer outputF.Close() //nolint:errcheck // temporary file for testing
+
+	var stdout bytes.Buffer
+	getenv := getEnvFunc(t, map[string]string{
+		"GITHUB_STEP_SUMMARY": summaryF.Name(),
+		"GITHUB_OUTPUT":       outputF.Name(),
+	})
+	action := githubactions.New(githubactions.WithGetenv(getenv), githubactions.WithWriter(&stdout))
+
+	version := "0.100.0~main"
+
+	setResults(action, version)
+
+	expected := "version: 0.100.0~main\n"
+	assert.Equal(t, expected, stdout.String(), "stdout does not match")
+
+	b, err := io.ReadAll(summaryF)
+	require.NoError(t, err)
+	assert.Equal(t, expected, string(b), "summary does not match")
+
+	expectedOutput := `
+version<<_GitHubActionsFileCommandDelimeter_
+0.100.0~main
+_GitHubActionsFileCommandDelimeter_
+`[1:]
+	b, err = io.ReadAll(outputF)
+	require.NoError(t, err)
+	assert.Equal(t, expectedOutput, string(b), "output parameters does not match")
+}
+
+func TestControlVersion(t *testing.T) {
+	dir := t.TempDir()
+
+	controlF, err := os.CreateTemp(dir, "test.control")
+	defer controlF.Close() //nolint:errcheck // temporary file for testing
+
+	buf := `comment = 'API surface for DocumentDB for PostgreSQL'
+default_version = '0.100-0'
+module_pathname = '$libdir/pg_documentdb'
+relocatable = false
+superuser = true
+requires = 'documentdb_core, pg_cron, tsm_system_rows, vector, postgis, rum'`
+	_, err = io.WriteString(controlF, buf)
+	require.NoError(t, err)
+
+	version, err := controlVersion(controlF.Name())
+	require.NoError(t, err)
+
+	require.Equal(t, "0.100-0", version)
+}
