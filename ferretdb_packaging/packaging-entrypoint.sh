fe0cb8503197c47307c185a2c043bb151619503c
Chi Fujii <chi.fujii@ferretdb.io>
2025-02-26 10:58:13 +0900

    simplify file renaming expression

--- ferretdb_packaging/packaging-entrypoint.sh
+++ ferretdb_packaging/packaging-entrypoint.sh
@@ -12,14 +12,17 @@ sed -i '/internal/d' Makefile
 # Build the Debian package
 debuild -us -uc
 
+# Change to the root to make file renaming expression simpler
+cd /
+
 # Rename .deb files to include the OS name prefix
-for f in ../*.deb; do mv $f $(echo $f | sed "s/\(.*\)\//\1\/$OS-/g"); done
+for f in *.deb; do mv $f $(echo $f | sed "s/^/$OS-/"); done
 
 # Create the output directory if it doesn't exist
 mkdir -p /output
 
 # Copy the built packages to the output directory
-cp ../*.deb /output/
+cp *.deb /output/
 
 # Change ownership of the output files to match the host user's UID and GID
 chown -R $(stat -c "%u:%g" /output) /output
