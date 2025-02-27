8fe521952aa7d8da9ffd813ad3bdea05f60c35c2
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2025-02-27 17:58:24 +0400

    Small tweaks (#17)

--- ferretdb_packaging/defineversion/main.go
+++ ferretdb_packaging/defineversion/main.go
@@ -202,7 +202,7 @@ func definePackagerVersionForTag(tag string) (string, error) {
 
 // setResults sets action output parameters, summary, etc.
 func setResults(action *githubactions.Action, res string) {
-	output := fmt.Sprintf("version: %s", res)
+	output := fmt.Sprintf("version: `%s`", res)
 
 	action.AddStepSummary(output)
 	action.Infof("%s", output)
